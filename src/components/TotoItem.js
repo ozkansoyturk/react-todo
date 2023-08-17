1c4c9d966a1fe6dda6211889f07eb618ae632368
ozkansoyturk <soyturkozkan@gmail.com>
2023-08-17 17:28:52 +0200

    Communication d'un composant parent vers un composant enfant

--- src/components/TotoItem.js
+++ src/components/TotoItem.js
@@ -1,10 +1,12 @@
 function TodoItem({ todo, deleteTodo }) {
   return (
-    <li>
-      <span>{todo.content}</span>
-      <button>Valider</button>
-      <button>Modifier</button>
-      <button onClick={() => deleteTodo(todo.id)}>Supprimer</button>
+    <li className="mb-10 d-flex justify-content-center align-items-center p-10">
+      <span className="flex-fill">{todo.content}</span>
+      <button className="btn btn-primary mr-15">Valider</button>
+      <button className="btn btn-primary mr-15">Modifier</button>
+      <button className="btn btn-reverse-primary" onClick={deleteTodo}>
+        Supprimer
+      </button>
     </li>
   );
 }
